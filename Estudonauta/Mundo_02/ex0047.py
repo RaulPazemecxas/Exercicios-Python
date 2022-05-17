06d6c6b58ebbee86c687b7c75f7e65e5c9ead214
Raul Pazemécxas de Andrade <raul_pazemecxas@hotmail.com>
2022-05-17 17:33:35 -0400

    treinando estrutura de repetição for

---
+++ Estudonauta/Mundo_02/ex0047.py
@@ -0,0 +1,7 @@
+#crie um programa que mostre na tela todos os numeros pares de 1 a 50
+from time import sleep
+
+for c in range(50, -2, -2):
+    print(c)
+    sleep(0.4)
+
---
+++ Estudonauta/Mundo_02/ex0048.py
@@ -0,0 +1,8 @@
+#faça um origrana qye cakcyke a sina ebtre tidis is byneris unoares qye sai nyktuokis de tres e qye se encontram
+#no intervalo de 1 ate 500
+s = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        s = s + c
+print(s)
+
